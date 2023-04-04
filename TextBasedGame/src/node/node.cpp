7fee4204135df6a66195bdcff66d6b900900a095
Konstantinos Giantselidis <konstantinosyants@gmail.com>
2023-04-04 14:45:53 +0300

    inaccessibleRoomDescription

--- TextBasedGame/src/node/node.cpp
+++ TextBasedGame/src/node/node.cpp
@@ -1,1 +1,10 @@
 #include "node.h"
+
+/**
+ * @brief Will be called when a player tries to enter an inacessible room
+ * 
+ * @return std::string The description of the room the player tried to enter
+ */
+std::string Node::inaccessibleRoomDescription(){
+            return this->description;
+        }
--- TextBasedGame/src/node/node.h
+++ TextBasedGame/src/node/node.h
@@ -47,6 +47,12 @@ class Node{
 
         ~Node(){}
 
-    //Auxiliary functions
+        //Auxiliary functions
 
+        /**
+         * @brief Will be called when a player tries to enter an inacessible room
+         * 
+         * @return std::string The description of the room the player tried to enter
+         */
+        std::string inaccessibleRoomDescription();
 };      
