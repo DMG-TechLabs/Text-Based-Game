1d65dc6b66bff79c60993dd5f41e1fdc14621b54
Konstantinos Despoinidis <kdesp2003@gmail.com>
2023-04-24 19:36:23 +0300

    achievementcollection removed from engine

--- TextBasedGame/src/achievements/achievement.h
+++ TextBasedGame/src/achievements/achievement.h
@@ -38,18 +38,3 @@ class Achievement{
 
 };
 
-class AchievementCollection{
-    public:
-        vector<Achievement*> achievements;
-
-        ~AchievementCollection(){}
-        AchievementCollection(){
-            initAchievements();
-        }
-
-        void initAchievements();
-        void printAchievements();
-        Achievement* nameToObject(string name);
-        string exportAchievements();
-        void importAchievements(string str);
-};
