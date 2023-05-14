4f5d9eac28f360e594607e4401f63fa514e013eb
Konstantinos Despoinidis <kdesp2003@gmail.com>
2023-05-14 03:39:59 +0300

    achievements in their own folder

--- TextBasedGame/src/achievements/achievement_collection.cpp
+++ TextBasedGame/src/achievements/achievement_collection.cpp
@@ -1,21 +1,5 @@
-#include "../Text-Based-Game-Engine/Engine/src/achievements/achievement.h"
-#include <iostream>
 
-class AchievementCollection{
-    public:
-        vector<Achievement*> achievements;
-
-        ~AchievementCollection(){}
-        AchievementCollection(){
-            initAchievements();
-        }
-
-        void initAchievements();
-        void printAchievements();
-        Achievement* nameToObject(string name);
-        string exportAchievements();
-        void importAchievements(string str);
-};
+#include "./achievement_collection.h"
 
 
 void AchievementCollection::initAchievements(){
---
+++ TextBasedGame/src/achievements/achievement_collection.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "../../Text-Based-Game-Engine/Engine/src/achievements/achievement.h"
+#include <iostream>
+
+class AchievementCollection{
+    public:
+        vector<Achievement*> achievements;
+
+        ~AchievementCollection(){}
+        AchievementCollection(){
+            initAchievements();
+        }
+
+        void initAchievements();
+        void printAchievements();
+        Achievement* nameToObject(string name);
+        string exportAchievements();
+        void importAchievements(string str);
+};
