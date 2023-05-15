0acd559de79a00a6bd2d4e76219d90b4423ac194
Konstantinos Despoinidis <kdesp2003@gmail.com>
2023-05-15 17:02:47 +0300

    add a room to map with constructor

--- TextBasedGame/src/map/map.h
+++ TextBasedGame/src/map/map.h
@@ -6,13 +6,22 @@ class Map{
     public:
         vector<Node*> nodes;
 
-        Map(){}
+        Map(){
+            nodes.push_back(new Node(0, "Hall", true));
+
+            initMap();
+        }
         Map(int day){
+            nodes.push_back(new Node(0, "Hall", true));
+
             initMap(day);
         }
         ~Map(){}
 
         void initMap(int day);
-
+        void initMap();
+        Node *getNode(int id);
         void addNode(Node *node, vector<int> connectedTo = {});
+
+        void printMap();
 };   
